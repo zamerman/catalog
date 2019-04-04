8487fbf41d8394fb78b423b1e506bc85b8476ed9
Zachary Amerman <leetong.amerman@gmail.com>
2019-04-04 17:58:58 -0400

    Commented out unimplemented paths and added code for accessing database.

--- item_catalog.py
+++ item_catalog.py
@@ -5,30 +5,36 @@ app = Flask(__name__)
 # import sql methods and classes from sqlalchemy library
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from catalog_setup import Base, Category, Gear
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 # app home page
 @app.route('/')
 @app.route('/catalog/')
-
-
-# app categories catalog page
-@app.route('/catalog/<string:category>/items/')
-
-# app item information page
-@app.route('/catalog/<string:category>/<string:item_name>')
-
-# app item creation page
-@app.route('/catalog/create/')
-
-# app item editer page
-@app.route('/catalog/<string:item_name>/edit/')
-
-# app item deleter page
-@app.route('/catalog/<string:item_name>/delete/')
-
 def initialCatalog():
     return render_template('catalogpage.html')
-    
+
+# app categories catalog page
+#@app.route('/catalog/<string:category>/items/')
+
+# app item information page
+#@app.route('/catalog/<string:category>/<string:item_name>')
+
+# app item creation page
+#@app.route('/catalog/create/')
+
+# app item editer page
+#@app.route('/catalog/<string:item_name>/edit/')
+
+# app item deleter page
+#@app.route('/catalog/<string:item_name>/delete/')
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
