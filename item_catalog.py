8a93166eeb274d3f3255af982323ad3d55eaa656
Zachary Amerman <leetong.amerman@gmail.com>
2019-04-03 21:17:28 -0400

    Referenced python server code to html template.

--- item_catalog.py
+++ item_catalog.py
@@ -1,5 +1,5 @@
 # import webpage framework methods and classes from flask library
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 # import sql methods and classes from sqlalchemy library
@@ -9,8 +9,7 @@ from sqlalchemy.orm import sessionmaker
 # app home page
 @app.route('/')
 @app.route('/catalog/')
-def initialCatalog():
-    return "Catalog"
+
 
 # app categories catalog page
 @app.route('/catalog/<string:category>/items/')
@@ -27,6 +26,9 @@ def initialCatalog():
 # app item deleter page
 @app.route('/catalog/<string:item_name>/delete/')
 
+def initialCatalog():
+    return render_template('catalogpage.html')
+    
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
