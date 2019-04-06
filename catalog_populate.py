497066f92f2b86e583f3f9d2c00769df0160d3ef
Zachary Amerman <leetong.amerman@gmail.com>
2019-04-06 16:41:37 -0400

    Code now conforms to PEP8 style also added comments

--- catalog_populate.py
+++ catalog_populate.py
@@ -1,28 +1,39 @@
+# Import database classes and functions
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from catalog_setup import Base, Category, Gear
 
+# Import datetime class and methods
 from datetime import datetime
 
+# Create database engine and bind it to Base
 engine = create_engine('sqlite:///catalog.db')
-Base.metadata.bind=engine
+Base.metadata.bind = engine
 
+# Create database sessions
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Store some initial category names
 categories = ["Soccer", "Basketball", 'Baseball', 'Frisbee', 'Snowboarding',
-        'Rock Climbing', 'Football', 'Skating', 'Hockey']
+              'Rock Climbing', 'Football', 'Skating', 'Hockey']
 
-gear = {'Soccerball':['ball for playing soccer', 'Soccer'],
-        'Snowboard':['board for snowboarding', 'Snowboarding']}
+# Store some initial gear
+gear = {'Soccerball': ['ball for playing soccer', 'Soccer'],
+        'Snowboard': ['board for snowboarding', 'Snowboarding']}
 
+# Insert categories into category table in database
 for category in categories:
     newCategory = Category(name=category)
     session.add(newCategory)
     session.commit()
 
+# Insert Gear into Gear table in database
 for item in gear:
-    category=session.query(Category).filter_by(name=gear[item][1]).one()
-    newGear = Gear(name=item, description=gear[item][0], datetimeadded=datetime.now(), category=category)
+    category = session.query(Category).filter_by(name=gear[item][1]).one()
+    newGear = Gear(name=item,
+                   description=gear[item][0],
+                   datetimeadded=datetime.now(),
+                   category=category)
     session.add(newGear)
     session.commit()
--- catalog_setup.py
+++ catalog_setup.py
@@ -1,16 +1,22 @@
+# Import sqlalchemy database classes and methods
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+# Create base class
 Base = declarative_base()
 
+
+# Create Category class
 class Category(Base):
     __tablename__ = 'category'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
 
+
+# Create Gear class
 class Gear(Base):
     __tablename__ = 'gear'
 
@@ -21,6 +27,9 @@ class Gear(Base):
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
 
+
+# Create engine for catalog
 engine = create_engine('sqlite:///catalog.db')
 
+# Create database
 Base.metadata.create_all(engine)
